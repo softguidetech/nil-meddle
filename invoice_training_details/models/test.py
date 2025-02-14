c6e0704bcacbcb33ef3178603c908d88b776fc7c
rkhattam1995 <rs.khattam@gmail.com>
2025-02-14 13:35:31 +0400

    Delete invoice_training_details/models/test.py

--- invoice_training_details/models/test.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
-
-    custom_field_1 = fields.Char(string="Custom Field 1")
-    custom_field_2 = fields.Char(string="Custom Field 2")

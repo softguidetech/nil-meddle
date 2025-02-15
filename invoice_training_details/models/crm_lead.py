318588f94886605579c4778b90ec4e8108116525
rkhattam1995 <rs.khattam@gmail.com>
2025-02-15 13:24:31 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -5,12 +5,8 @@ from odoo import fields, models, api
 
 class Lead(models.Model):
     _inherit = 'crm.lead'
-    stage_name = fields.Char('Stage Name', compute='_compute_stage_name')
+    stage_id = fields.Many2one('crm.stage', string="Stage")
     
-        @api.depends('stage_id')
-        def _compute_stage_name(self):
-            for record in self:
-                record.stage_name = record.stage_id.name
     training_name = fields.Char(string='Training Name')
     service_name = fields.Char(string='Service Name')
     total_training_price = fields.Float(string='Total Training Price', compute="_compute_training_price", store=True)

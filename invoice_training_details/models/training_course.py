eaf7807ac0e8771ecd7ca6599ce960aad6860664
rkhattam1995 <rs.khattam@gmail.com>
2025-02-17 10:05:25 +0400

    Update training_course.py

--- invoice_training_details/models/training_course.py
+++ invoice_training_details/models/training_course.py
@@ -1,43 +1,38 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from odoo import api, fields, models, tools
-from datetime import datetime
-from odoo.exceptions import ValidationError
+from odoo import api, fields, models
+from datetime import timedelta
 
 class TrainingCourse(models.Model):
     _name = "training.course"
     _description = 'Training Course'
 
-    name = fields.Char(string='Training Name',)
-    no_of_student = fields.Integer(string='#of Student')
-    duration = fields.Char(string='Duration',compute='_compute_date')
+    name = fields.Char(string='Training Name')
+    no_of_student = fields.Integer(string='# of Students')
+    duration = fields.Integer(string='Duration (Days)', compute='_compute_date', store=True)
     training_date_start = fields.Date(string='Start Date')
-    training_date_end = fields.Date(string='Delivery Date')
+    training_date_end = fields.Date(string='End Date')
     price = fields.Float(string='Selling Price')
     move_id = fields.Many2one('account.move', string='Move')
     lead_id = fields.Many2one('crm.lead', string='Lead')
     sale_id = fields.Many2one('sale.order', string='Sale Order')
     
-    instructor_id = fields.Many2one('hr.employee',string="Instructor")
+    instructor_id = fields.Many2one('hr.employee', string="Instructor")
     descriptions = fields.Char(string='Description')
-    training_id = fields.Many2one('product.product',string='Training Name')
+    training_id = fields.Many2one('product.product', string='Training Name')
     train_language = fields.Char(string='Language')
     
     where_location2 = fields.Char(string='Where?')
-    location = fields.Selection([('ILT','ILT'),('VILT','VILT')])
-    payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
+    location = fields.Selection([('ILT', 'ILT'), ('VILT', 'VILT')])
+    payment_method = fields.Selection([('cash', 'Cash'), ('clc', 'CLC')], default='cash')
     clcs_qty = fields.Float(string='CLCs Qty')
-    default_item_code = fields.Char(related='training_id.default_code',string='Internal Ref')
+    default_item_code = fields.Char(related='training_id.default_code', string='Internal Ref')
     
-    cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc',string="Cost Clc")
-    hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink',string="Hyper Link")
-    
+    cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc', string="Cost CLC")
+    hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink', string="Hyper Link")
+
+    @api.depends('training_date_start', 'training_date_end')
     def _compute_date(self):
-        
-        duration = 0
         for rec in self:
-            duration = rec.training_date_end - rec.training_date_start
-            days= str(duration).replace(', 0:00:00','')
-            rec.duration = days
-         
+            if rec.training_date_start and rec.training_date_end:
+                rec.duration = (rec.training_date_end - rec.training_date_start).days
+            else:
+                rec.duration = 0  # Default to 0 if dates are missing

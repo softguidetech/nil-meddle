8682654738888ee70f9ac698347e9c5e5725d56c
rkhattam1995 <rs.khattam@gmail.com>
2025-02-19 22:14:48 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -1,9 +1,7 @@
-
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import fields, models, api
-
 
 
 class Lead(models.Model):
@@ -12,34 +10,33 @@ class Lead(models.Model):
     training_name = fields.Char(string='Training Name')
     service_name = fields.Char(string='Service Name')
     total_training_price = fields.Float(string='Total Training Price', compute="_compute_training_price", store=True)
-    total_service_price = fields.Float(string='Total Servicr Price', compute="_compute_service_price", store=True)
+    total_service_price = fields.Float(string='Total Service Price', compute="_compute_service_price", store=True)
     half_advance_payment_before = fields.Float(string='Advance payment amount 50% (paid)')
     half_payment_after = fields.Float(string='50% Amount after Training Delivery (Not Yet Paid)')
     training_course_ids = fields.One2many('training.course', 'lead_id', string='Training Courses')
-    pro_service_ids = fields.One2many('pro.service','pro_lead_id',srting='Professional Services')
-    ticket_ids = fields.One2many('ticket.ticket','ticket_lead_id',string='Tickets')
-    hotel_ids = fields.One2many('hotel.hotel','hotel_lead_id',string='Hotels')
-    total_price_all = fields.Float(string="Total Amount",compute='_compute_total')
+    pro_service_ids = fields.One2many('pro.service','pro_lead_id', string='Professional Services')
+    ticket_ids = fields.One2many('ticket.ticket', 'ticket_lead_id', string='Tickets')
+    hotel_ids = fields.One2many('hotel.hotel', 'hotel_lead_id', string='Hotels')
+    total_price_all = fields.Float(string="Total Amount", compute='_compute_total')
     visa = fields.Boolean(string="Visa")
     start_date = fields.Date(string="From Date")
     to_date = fields.Date(string="To Date")
     book_details_id = fields.Many2many('ir.attachment', 'doc_attach_rel4', 'doc_id', 'attach_id5',
-                                         string="Booking Details",
-                                         help='You can attach the copy of your document', copy=False)
+                                        string="Booking Details", help='You can attach the copy of your document', copy=False)
     details = fields.Html(string="Details")
     cost = fields.Float(string="Cost")
     training_vendor = fields.Char(string="Training Vendor")
     training_type = fields.Char(string="Training Type")
     
-    #Add extera
-    instructor_id = fields.Many2one('hr.employee',string="Instructor")
+    # Add extra
+    instructor_id = fields.Many2one('hr.employee', string="Instructor")
     descriptions = fields.Char(string='Description')
-    ordering_partner_id = fields.Many2one('res.partner',string='Ordering Partner')
-    training_id = fields.Many2one('product.template',string='Training Name')
+    ordering_partner_id = fields.Many2one('res.partner', string='Ordering Partner')
+    training_id = fields.Many2one('product.template', string='Training Name')
     
     train_language = fields.Char(string='Training Language')
-    location = fields.Selection([('Cisco U','Cisco U'),('ILT','ILT'),('VILT','VILT')])
-    payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
+    location = fields.Selection([('Cisco U', 'Cisco U'), ('ILT', 'ILT'), ('VILT', 'VILT')])
+    payment_method = fields.Selection([('cash', 'Cash'), ('clc', 'CLC')], default='cash')
     clcs_qty = fields.Float(string='CLCs Qty')
     
     # extra information tab
@@ -47,35 +44,38 @@ class Lead(models.Model):
     so_no = fields.Char(string='SO#')
     tr_expiry_date = fields.Date(string='Expiry Date')
     
-    # 
+    # CLC cost and margin fields
     clc_cost = fields.Char(string="CLCs Cost")
     rate_card = fields.Float(string="Rate Card $")
     nilme_share = fields.Float(string="NIL ME Share $")
     
     # logistics tab
     instructor_logistics = fields.Char(string='Instructor Logistics')
-    catering = fields.Selection([('NIL MM','NIL MN'),('Others','Others')],string='Catering')
+    catering = fields.Selection([('NIL MM', 'NIL MN'), ('Others', 'Others')], string='Catering')
+    
+    # New CLC Fields
+    training_cost = fields.Float(string="Training Cost")
+    logistics_cost = fields.Float(string="Logistics Cost")
     
     def _compute_total(self):
-        ticket_total =0
-        hotel_toal=0
+        ticket_total = 0
+        hotel_total = 0
         cost = 0
         for rec in self:
             if rec.ticket_ids and rec.hotel_ids:
                 for ticket in rec.ticket_ids:
-                    ticket_total+=ticket.price
+                    ticket_total += ticket.price
                 for hotel in rec.hotel_ids:
-                    hotel_toal+=hotel.price
-                rec.total_price_all = ticket_total + hotel_toal + rec.cost
+                    hotel_total += hotel.price
+                rec.total_price_all = ticket_total + hotel_total + rec.cost
             else:
                 rec.total_price_all = 0
-    
+
     @api.depends('pro_service_ids.price')
     def _compute_service_price(self):
         for rec in self:
             if rec.pro_service_ids:
                 rec.total_service_price = sum(rec.pro_service_ids.mapped('price'))
-            
             else:
                 rec.total_service_price = 0
                 
@@ -91,8 +91,6 @@ class Lead(models.Model):
         quotation_context = super()._prepare_opportunity_quotation_context()
         quotation_context.update({
             'default_training_name': self.training_name,
-            # 'default_half_advance_payment_before': self.half_advance_payment_before,
-            # 'default_half_payment_after': self.half_payment_after,
             'default_training_course_ids': [(6, 0, self.training_course_ids.ids)],
             'default_pro_service_ids': [(6, 0, self.pro_service_ids.ids)],
             'default_clcs_qty': self.clcs_qty,
@@ -109,7 +107,6 @@ class Lead(models.Model):
             'default_payment_method': self.payment_method,
             'default_clcs_qty': self.clcs_qty,
             'default_service_name': self.service_name,
-            
             'default_hotel_ids': [(6, 0, self.hotel_ids.ids)],
             'default_ticket_ids': [(6, 0, self.ticket_ids.ids)],
             'default_visa': self.visa,
@@ -118,11 +115,11 @@ class Lead(models.Model):
             'default_book_details_id': [(6, 0, self.book_details_id.ids)],
             'default_details': self.details,
             'default_cost': self.cost,
-            
             'default_training_vendor': self.training_vendor,
             'default_training_type': self.training_type,
         })
         return quotation_context
+
 
 class HotelHotel(models.Model):
     _name = 'hotel.hotel'

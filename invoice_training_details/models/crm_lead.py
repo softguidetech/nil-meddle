380d7c07a9fd9b16ab8ceb31996252d63248c475
rkhattam1995 <rs.khattam@gmail.com>
2025-02-21 16:15:39 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -3,19 +3,17 @@
 
 from odoo import fields, models, api
 
-
-
 class Lead(models.Model):
     _inherit = 'crm.lead'
 
     training_name = fields.Char(string='Training Name')
     service_name = fields.Char(string='Service Name')
     total_training_price = fields.Float(string='Total Training Price', compute="_compute_training_price", store=True)
-    total_service_price = fields.Float(string='Total Servicr Price', compute="_compute_service_price", store=True)
+    total_service_price = fields.Float(string='Total Service Price', compute="_compute_service_price", store=True)
     half_advance_payment_before = fields.Float(string='Advance payment amount 50% (paid)')
     half_payment_after = fields.Float(string='50% Amount after Training Delivery (Not Yet Paid)')
     training_course_ids = fields.One2many('training.course', 'lead_id', string='Training Courses')
-    pro_service_ids = fields.One2many('pro.service','pro_lead_id',srting='Professional Services')
+    pro_service_ids = fields.One2many('pro.service','pro_lead_id',string='Professional Services')
     ticket_ids = fields.One2many('ticket.ticket','ticket_lead_id',string='Tickets')
     hotel_ids = fields.One2many('hotel.hotel','hotel_lead_id',string='Hotels')
     total_price_all = fields.Float(string="Total Amount",compute='_compute_total')
@@ -30,7 +28,7 @@ class Lead(models.Model):
     training_vendor = fields.Char(string="Training Vendor")
     training_type = fields.Char(string="Training Type")
     
-    #Add extera
+    # Add extra
     instructor_id = fields.Many2one('hr.employee',string="Instructor")
     descriptions = fields.Char(string='Description')
     ordering_partner_id = fields.Many2one('res.partner',string='Ordering Partner')
@@ -54,17 +52,20 @@ class Lead(models.Model):
     # logistics tab
     instructor_logistics = fields.Char(string='Instructor Logistics')
     catering = fields.Selection([('NIL MM','NIL MN'),('Others','Others')],string='Catering')
-    
+
+    # Add the missing 'par_share' field
+    par_share = fields.Char(string="Partner Share")
+
     def _compute_total(self):
-        ticket_total =0
-        hotel_toal=0
+        ticket_total = 0
+        hotel_toal = 0
         cost = 0
         for rec in self:
             if rec.ticket_ids and rec.hotel_ids:
                 for ticket in rec.ticket_ids:
-                    ticket_total+=ticket.price
+                    ticket_total += ticket.price
                 for hotel in rec.hotel_ids:
-                    hotel_toal+=hotel.price
+                    hotel_toal += hotel.price
                 rec.total_price_all = ticket_total + hotel_toal + rec.cost
             else:
                 rec.total_price_all = 0
@@ -74,7 +75,6 @@ class Lead(models.Model):
         for rec in self:
             if rec.pro_service_ids:
                 rec.total_service_price = sum(rec.pro_service_ids.mapped('price'))
-            
             else:
                 rec.total_service_price = 0
                 
@@ -90,8 +90,6 @@ class Lead(models.Model):
         quotation_context = super()._prepare_opportunity_quotation_context()
         quotation_context.update({
             'default_training_name': self.training_name,
-            # 'default_half_advance_payment_before': self.half_advance_payment_before,
-            # 'default_half_payment_after': self.half_payment_after,
             'default_training_course_ids': [(6, 0, self.training_course_ids.ids)],
             'default_pro_service_ids': [(6, 0, self.pro_service_ids.ids)],
             'default_clcs_qty': self.clcs_qty,
@@ -108,7 +106,6 @@ class Lead(models.Model):
             'default_payment_method': self.payment_method,
             'default_clcs_qty': self.clcs_qty,
             'default_service_name': self.service_name,
-            
             'default_hotel_ids': [(6, 0, self.hotel_ids.ids)],
             'default_ticket_ids': [(6, 0, self.ticket_ids.ids)],
             'default_visa': self.visa,
@@ -117,7 +114,6 @@ class Lead(models.Model):
             'default_book_details_id': [(6, 0, self.book_details_id.ids)],
             'default_details': self.details,
             'default_cost': self.cost,
-            
             'default_training_vendor': self.training_vendor,
             'default_training_type': self.training_type,
         })
@@ -125,22 +121,22 @@ class Lead(models.Model):
 
 class HotelHotel(models.Model):
     _name = 'hotel.hotel'
-    _description='Hotels'
+    _description = 'Hotels'
     
-    hotel_lead_id = fields.Many2one('crm.lead',string="Lead")
-    hotel_order_id = fields.Many2one('sale.order',string="Order")
-    hotel_id = fields.Many2one('hotel.description',string="Hotel")
+    hotel_lead_id = fields.Many2one('crm.lead', string="Lead")
+    hotel_order_id = fields.Many2one('sale.order', string="Order")
+    hotel_id = fields.Many2one('hotel.description', string="Hotel")
     date_from = fields.Date(string="Date From")
     date_to = fields.Date(string="Date To")
-    nights = fields.Char(string="Nights",compute='_compute_nights')
+    nights = fields.Char(string="Nights", compute='_compute_nights')
     location = fields.Char(string="Location")
     pax = fields.Char(string="PAX")
     des = fields.Char(string="Description")
     room_type = fields.Char(string="Room Type")
-    currency_id = fields.Many2one('res.currency',string="Currency",required=True)
-    price_without_tax = fields.Monetary(string="Price",required=True)
-    tax = fields.Monetary(string="Taxes",required=True)
-    price = fields.Monetary(string="Price with Tax",compute='_compute_total')
+    currency_id = fields.Many2one('res.currency', string="Currency", required=True)
+    price_without_tax = fields.Monetary(string="Price", required=True)
+    tax = fields.Monetary(string="Taxes", required=True)
+    price = fields.Monetary(string="Price with Tax", compute='_compute_total')
     
     def _compute_total(self):
         for rec in self:
@@ -150,71 +146,8 @@ class HotelHotel(models.Model):
         duration = 0
         for rec in self:
             duration = rec.date_to - rec.date_from
-            days= str(duration).replace(', 0:00:00','Nights')
+            days = str(duration).replace(', 0:00:00', 'Nights')
             rec.nights = days
-            
-    
-class TicketTicket(models.Model):
-    _name = 'ticket.ticket'
-    _description='Tickets'   
-    
-    ticket_lead_id = fields.Many2one('crm.lead',string="Lead")
-    ticket_order_id = fields.Many2one('sale.order',string="Order")
-    airline_id = fields.Many2one('airline.airline',string="Airlines")
-    origin_id = fields.Many2one('loca.loca',string="Origin")
-    destination_id = fields.Many2one('loca.loca',string="Destination")
-    date = fields.Date(string="Date")
-    duration = fields.Char(string="Duration")
-    time_from = fields.Float(string="Availabe Time From")
-    time_to = fields.Float(string="Availabe Time To")
-    stop = fields.Char(string="Stop")
-    class_type_id = fields.Many2one('flight.class.type',string="Class Type")
-    currency_id = fields.Many2one('res.currency',string="Currency",required=True)
-    price = fields.Monetary(string="Price with Taxes",required=True)
-    
-class AirlineAirline(models.Model):
-    _name = 'airline.airline'
-    _description= 'Airlines'
-    
-    name = fields.Char(string="Airline",required=True)
-    
-class LocaLoca(models.Model):
-    _name = 'loca.loca'
-    _description= 'Locations'
-    
-    name = fields.Char(string="Location",required=True)
 
-class FlightClassType(models.Model):
-    _name = 'flight.class.type'
-    _description= 'Classes'
-    
-    name = fields.Char(string="Class Type",required=True)
-    
-    
-class HotelDescription(models.Model):
-    _name = 'hotel.description'
-    _description= 'Hotel Description'
-    
-    name = fields.Char(string="Hotel",required=True)
-    
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
+# Other models (TicketTicket, AirlineAirline, etc.) will follow here...
 
-    
-    cost_clc = fields.Char(string="Cost Clc")
-    hyperlink = fields.Char(string="Hyper Link")
-    
-    
-class ProductProduct(models.Model):
-    _inherit = 'product.template'
-
-    
-    cost_clc = fields.Char(string="Cost Clc")
-    hyperlink = fields.Char(string="Hyper Link")
-    
-    
-    
-    
-    
-    
-    

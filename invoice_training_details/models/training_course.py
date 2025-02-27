6519b350da0bd65f2d016308f9e61bb22f60bb51
rkhattam1995 <rs.khattam@gmail.com>
2025-02-28 00:44:26 +0400

    Update training_course.py

--- invoice_training_details/models/training_course.py
+++ invoice_training_details/models/training_course.py
@@ -11,7 +11,7 @@ class TrainingCourse(models.Model):
 
     name = fields.Char(string='Training Name',)
     no_of_student = fields.Integer(string='# of Student')
-    duration = fields.Integer(string='Duration (days)', compute='_compute_date')
+    duration = fields.Integer(string='Duration (days)', compute='_compute_date', store=True)
     training_date_start = fields.Date(string='Start Date')
     training_date_end = fields.Date(string='End Date')
     price = fields.Float(string='Selling Price')
@@ -44,6 +44,7 @@ class TrainingCourse(models.Model):
     cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc', string="CLC Cost")
     hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink', string="Link")
 
+    @api.depends('training_date_start', 'training_date_end')
     def _compute_date(self):
         for rec in self:
             if rec.training_date_start and rec.training_date_end:

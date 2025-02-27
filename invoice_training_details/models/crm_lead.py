288fa506a9367a206da6720aa66ec71d62311e11
rkhattam1995 <rs.khattam@gmail.com>
2025-02-27 12:30:33 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -39,6 +39,7 @@ class Lead(models.Model):
     train_language = fields.Char(string='Training Language')
     location = fields.Selection([('Cisco U','Cisco U'),('ILT','ILT'),('VILT','VILT')])
     payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
+    clcs_qty = fields.Float(string='CLCs Qty')
     
     # extra information tab
     clcs_qty = fields.Float(string='CLCs Qty')

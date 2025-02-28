5ee08b7e604489f7176171474a243d148153a912
rkhattam1995 <rs.khattam@gmail.com>
2025-02-28 13:38:11 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -9,6 +9,7 @@ class Lead(models.Model):
     _inherit = 'crm.lead'
 
     training_name = fields.Char(string='Training Name')
+    kanb_view = fields.char(string='Training Name')
     service_name = fields.Char(string='Service Name')
     total_training_price = fields.Float(string='Total Training Price', compute="_compute_training_price", store=True)
     total_service_price = fields.Float(string='Total Servicr Price', compute="_compute_service_price", store=True)

c24857416cad686aa76bcec94562467cd1fb0022
rkhattam1995 <rs.khattam@gmail.com>
2025-02-28 13:57:45 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -8,7 +8,12 @@ from odoo import fields, models, api
 class Lead(models.Model):
     _inherit = 'crm.lead'
 
-    training_name = fields.Char(string='Training Name')
+@api.depends('training_name')
+def _compute_new_training_name(self):
+    for record in self:
+        record.new_training_name = record.training_name
+
+    new_training_name = fields.Char(string='New Training Name', compute='_compute_new_training_name', store=True)
     service_name = fields.Char(string='Service Name')
     total_training_price = fields.Float(string='Total Training Price', compute="_compute_training_price", store=True)
     total_service_price = fields.Float(string='Total Servicr Price', compute="_compute_service_price", store=True)

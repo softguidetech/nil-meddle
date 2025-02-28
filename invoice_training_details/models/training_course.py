5eb893fea4446e10a5dff9ab62ff1938cbf1a4f7
rkhattam1995 <rs.khattam@gmail.com>
2025-02-28 14:30:47 +0400

    Update training_course.py

--- invoice_training_details/models/training_course.py
+++ invoice_training_details/models/training_course.py
@@ -10,11 +10,11 @@ class TrainingCourse(models.Model):
     _description = 'Training Course'
 
     name = fields.Char(string='Training Name',)
-    no_of_student = fields.Integer(string='#of Student')
+    no_of_student = fields.Integer(string='No of Student')
     duration = fields.Char(string='Duration',compute='_compute_date')
-    training_date_start = fields.Date(string='Start Date')
-    training_date_end = fields.Date(string='Delivery Date')
-    price = fields.Float(string='Selling Price')
+    training_date_start = fields.Date(string='Training Date start')
+    training_date_end = fields.Date(string='Training Date end')
+    price = fields.Float(string='Training Price')
     move_id = fields.Many2one('account.move', string='Move')
     lead_id = fields.Many2one('crm.lead', string='Lead')
     sale_id = fields.Many2one('sale.order', string='Sale Order')
@@ -22,31 +22,16 @@ class TrainingCourse(models.Model):
     instructor_id = fields.Many2one('hr.employee',string="Instructor")
     descriptions = fields.Char(string='Description')
     training_id = fields.Many2one('product.product',string='Training Name')
-    train_language = fields.Char(string='Language')
-    # Add this computed field
-    stage_id = fields.Many2one(
-        'crm.stage', 
-        string='Stage', 
-        compute='_compute_stage',
-        store=False
-    )
-
-    # Add this compute method
-    @api.depends('lead_id.stage_id')
-    def _compute_stage(self):
-        for rec in self:
-            rec.stage_id = rec.lead_id.stage_id
-
-    # Rest of your existing fields...
+    train_language = fields.Char(string='Training Language')
     
     where_location2 = fields.Char(string='Where?')
-    location = fields.Selection([('ILT','ILT'),('VILT','VILT')])
+    location = fields.Selection([('CISCO U','CISCO U'),('ILT','ILT'),('VILT','VILT')])
     payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
     clcs_qty = fields.Float(string='CLCs Qty')
     default_item_code = fields.Char(related='training_id.default_code',string='Internal Ref')
     
-    cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc',string="CLC Cost")
-    hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink',string="Link")
+    cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc',string="Cost Clc")
+    hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink',string="Hyper Link")
     
     def _compute_date(self):
         

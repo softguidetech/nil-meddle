e0a60029cd84e8aa709de1b557711ab734bb5fb9
rkhattam1995 <rs.khattam@gmail.com>
2025-03-10 01:57:29 +0400

    Update cost_details.py

--- invoice_training_details/models/cost_details.py
+++ invoice_training_details/models/cost_details.py
@@ -16,22 +16,20 @@ class CostDetails(models.Model):
     margin1 = fields.Float(string="Total Costs", compute='_compute_margin1')
     clc_cost = fields.Float(string="Training Cost")
     rate_card = fields.Float(string="Partner Rate")  
-    nilme_share = fields.Float(string="NIL ME Share $")
+    nilme_share = fields.Float(string="NIL ME Share $", compute='_compute_nilme_share')
     learning_partner = fields.Selection([
         ('Koeing', 'Koeing'),
-        ('Mira','Mira'),
+        ('Mira', 'Mira'),
         ('NIL LTD', 'NIL LTD'),
         ('NIL SA', 'NIL SA')
     ], string='Learning Partner')
 
-   @api.depends('training_vendor', 'total_price_all', 'clc_cost')
+    @api.depends('training_vendor', 'total_price_all', 'clc_cost')
     def _compute_margin1(self):
         for record in self:
             record.margin1 = (record.training_vendor or 0) + (record.total_price_all or 0) + (record.clc_cost or 0)
 
     @api.depends('margin1', 'cos_lead_id.total_training_price')
     def _compute_nilme_share(self):
-        for record in self:
-            record.nilme_share = (record.cos_lead_id.total_training_price or 0) - (record.margin1 or 0)
         for record in self:
             record.nilme_share = (record.cos_lead_id.total_training_price or 0) - (record.margin1 or 0)

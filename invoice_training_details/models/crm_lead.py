e0ebe2012c0f8a42e59f0dfb7bf46e63dd8edc0c
rkhattam1995 <rs.khattam@gmail.com>
2025-03-09 21:36:50 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -51,6 +51,7 @@ class Lead(models.Model):
             'nilme_share': 20.0,
         })
 
+
     train_language = fields.Char(string='Language')
     location = fields.Selection([('ILT','ILT'),('VILT','VILT')])
     payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
@@ -77,19 +78,18 @@ class Lead(models.Model):
     total_price_all = fields.Float(string="Total Price All", compute="_compute_total", store=True, tracking=True)
 
     @api.depends('ticket_ids.price', 'hotel_ids.price', 'cost_details_ids.price', 
-                 'instructor_logistics', 'venue', 'ctrng', 'uber')
+                'venue', 'ctrng', 'uber')
     def _compute_total(self):
         for rec in self:
             ticket_total = sum(ticket.price for ticket in rec.ticket_ids) if rec.ticket_ids else 0
             hotel_total = sum(hotel.price for hotel in rec.hotel_ids) if rec.hotel_ids else 0
             cost_details_total = sum(cost.price for cost in rec.cost_details_ids) if rec.cost_details_ids else 0
-            instructor_logistics = float(rec.instructor_logistics) if rec.instructor_logistics else 0
             venue = rec.venue if rec.venue else 0
             catering = rec.ctrng if rec.ctrng else 0
             uber = rec.uber if rec.uber else 0
 
             rec.total_price_all = (ticket_total + hotel_total + cost_details_total + 
-                                   instructor_logistics + venue + catering + uber)
+                                  venue + catering + uber)
 
 
     

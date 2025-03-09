a61948a78a349e1eedf1b950ed0138b3d0e3a02a
rkhattam1995 <rs.khattam@gmail.com>
2025-03-10 02:08:48 +0400

    Update cost_details.py

--- invoice_training_details/models/cost_details.py
+++ invoice_training_details/models/cost_details.py
@@ -24,6 +24,42 @@ class CostDetails(models.Model):
         ('NIL SA', 'NIL SA')
     ], string='Learning Partner')
 
+    # Define the missing fields
+    ticket_ids = fields.One2many('ticket.model', 'cost_id', string="Tickets")
+    hotel_ids = fields.One2many('hotel.model', 'cost_id', string="Hotels")
+    cost_details_ids = fields.One2many('cost.details', 'cos_lead_id', string="Cost Details")
+    venue = fields.Float(string="Venue")
+    ctrng = fields.Float(string="Catering")
+    uber = fields.Float(string="Uber")
+
+    # Additional fields
+    hotel_lead_id = fields.Many2one('crm.lead', string="Lead")
+    hotel_order_id = fields.Many2one('sale.order', string="Order")
+    hotel_id = fields.Many2one('hotel.description', string="Hotel")
+    date_from = fields.Date(string="Date From")
+    date_to = fields.Date(string="Date To")
+    nights = fields.Char(string="Nights", compute='_compute_nights')
+    location = fields.Char(string="Location")
+    pax = fields.Char(string="PAX")
+    des = fields.Char(string="Description")
+    room_type = fields.Char(string="Room Type")
+    price_without_tax = fields.Monetary(string="Price", required=True)
+    tax = fields.Monetary(string="Taxes", required=True)
+    price_with_tax = fields.Monetary(string="Price with Tax", compute='_compute_total')
+    ticket_lead_id = fields.Many2one('crm.lead', string="Lead")
+    ticket_order_id = fields.Many2one('sale.order', string="Order")
+    airline_id = fields.Many2one('airline.airline', string="Airlines")
+    origin_id = fields.Many2one('loca.loca', string="Origin")
+    destination_id = fields.Many2one('loca.loca', string="Destination")
+    date = fields.Date(string="Date")
+    duration = fields.Char(string="Duration")
+    time_from = fields.Float(string="Available Time From")
+    time_to = fields.Float(string="Available Time To")
+    stop = fields.Char(string="Stop")
+    class_type_id = fields.Many2one('flight.class.type', string="Class Type")
+    currency_id = fields.Many2one('res.currency', string="Currency", required=True)
+    price_with_taxes = fields.Monetary(string="Price with Taxes", required=True)
+
     @api.depends('training_vendor', 'total_price_all', 'clc_cost', 'ticket_ids', 'hotel_ids', 'cost_details_ids', 'venue', 'ctrng', 'uber')
     def _compute_margin1(self):
         for record in self:

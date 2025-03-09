4f9d564205789d39f8c49f6c90681784846d4c85
rkhattam1995 <rs.khattam@gmail.com>
2025-03-09 14:02:09 +0400

    Update cost_details.py

--- invoice_training_details/models/cost_details.py
+++ invoice_training_details/models/cost_details.py
@@ -16,6 +16,7 @@ class CostDetails(models.Model):
     clc_cost = fields.Float(string="Training Cost")
     rate_card = fields.Float(string="Partner Rate")  
     nilme_share = fields.Float(string="NIL ME Share $")
+    price = fields.Float(string="Price")
 
     @api.depends('clc_cost', 'rate_card', 'price')
     def _compute_ttl_costs(self):

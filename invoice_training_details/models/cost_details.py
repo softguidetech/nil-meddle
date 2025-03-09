dec9647f21fe0642fb322ee5e6393ba68e7ca323
rkhattam1995 <rs.khattam@gmail.com>
2025-03-10 01:14:39 +0400

    Update cost_details.py

--- invoice_training_details/models/cost_details.py
+++ invoice_training_details/models/cost_details.py
@@ -7,11 +7,11 @@ class CostDetails(models.Model):
     cos_lead_id = fields.Many2one('crm.lead', string="Lead", ondelete='cascade')
     name = fields.Char(string="Cost Name", required=True)
     description = fields.Text(string="Description")
-    price = fields.Float(string="Price", required=True)
+    price = fields.Float(string="Price")  # Make the price field optional
     currency_id = fields.Many2one('res.currency', string="Currency", required=True, default=lambda self: self.env.company.currency_id.id)
 
     # ✅ These cost fields now belong only to cost.details
-    training_vendor = fields.Float(string="Vendor Share")  
+    training_vendor = fields.Float(string="Partner Share")  
     total_price_all = fields.Float(string="Logistics Cost")  
     margin1 = fields.Float(string="Margin 1", compute='_compute_margin1')
     clc_cost = fields.Float(string="Training Cost")
@@ -19,6 +19,7 @@ class CostDetails(models.Model):
     nilme_share = fields.Float(string="NIL ME Share $")
     learning_partner = fields.Selection([
         ('Koeing', 'Koeing'),
+        ('Mira','Mira'),
         ('NIL LTD', 'NIL LTD'),
         ('NIL SA', 'NIL SA')
     ], string='Learning Partner')

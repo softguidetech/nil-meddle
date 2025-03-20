8200e8085e297d98ff86c8885355cf2845aa47f8
rkhattam1995 <rs.khattam@gmail.com>
2025-03-20 16:15:23 +0400

    Update pro_service.py

--- invoice_training_details/models/pro_service.py
+++ invoice_training_details/models/pro_service.py
@@ -10,10 +10,6 @@ class ProService(models.Model):
     _description = 'Propfessional Services'
 
     name = fields.Char(string='Service Name',)
-    requester = fields.Char(string='Requester')
-    end_customer = fields.Char(string='End Client')
-    cisco_am = fields.Char(string='Cisco Account Manager')
-    
     no_of_student = fields.Integer(string='No of Student')
     duration = fields.Char(string='Duration',)
     training_date_start = fields.Date(string='Training Date start')
@@ -34,7 +30,9 @@ class ProService(models.Model):
     payment_method = fields.Selection([('cash','Cash'),('clc','CLC')],default='cash')
     clcs_qty = fields.Float(string='CLCs Qty')
     default_item_code = fields.Char(related='training_id.default_code',string='Internal Ref')
-    
+    requester = fields.Char(string='Requester')
+    end_customer = fields.Char(string='End Client')
+    cisco_am = fields.Char(string='Cisco Account Manager')
     cost_clc = fields.Char(related='training_id.product_tmpl_id.cost_clc',string="Cost Clc")
     hyperlink = fields.Char(related='training_id.product_tmpl_id.hyperlink',string="Hyper Link")
     

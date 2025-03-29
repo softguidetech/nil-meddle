4755fb0e9438d1d0fdb25d5a28d81a828eac9cd1
rkhattam1995 <rs.khattam@gmail.com>
2025-03-29 21:25:43 +0400

    Delete hr_custom/models/hr_attendance.py

--- hr_custom/models/hr_attendance.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class HrAttendance(models.Model):
-    _inherit = 'hr.attendance'
-
-    checkin_comment_ids = fields.One2many('hr.checkin.comment', 'attendance_id', string="Check-in Comments")

bd8a420d01e8f4fa2ea0d80333472a6d60e2a50a
rkhattam1995 <rs.khattam@gmail.com>
2025-03-29 17:56:16 +0400

    Create hr.checkin.comment.py

---
+++ hr_custom/hr.checkin.comment.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class HrCheckinComment(models.Model):
+    _name = 'hr.checkin.comment'
+    _description = 'Check-in Comment'
+
+    attendance_id = fields.Many2one('hr.attendance', string="Attendance", required=True)
+    comment = fields.Text(string="Comment", required=True)

8b3a8279b8911ab4546d9624cf8cb83484d248c0
rkhattam1995 <rs.khattam@gmail.com>
2025-03-29 17:32:40 +0400

    Update __manifest__.py

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -1,21 +1,26 @@
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
 {
     "name": "Invoice Training Details",
     "version": "17.0",
-    "summary": "Invoice Training Details",
-    "description": """
-       Invoice Training Details.
-    """,
-    "category": 'Customization',
-
-    # Author
-    "author": "",
+    "summary": "Enhancements for Invoice Training Details",
+    "description": "Customizations for invoicing related to training services, including improved reporting and CRM integration.",
+    "category": "Customization",
+    
+    # Author Information
+    "author": "SoftGuide Tech",
     "website": "https://www.softguidetech.com",
     "license": "LGPL-3",
+    
+    # Dependencies
+    "depends": [
+        "account", 
+        "crm", 
+        "sale_management", 
+        "hr", 
+        "product"
+    ],
 
-    # Dependency
-    "depends": ['account', 'crm', 'sale_management','hr','product'],
-
+    # Data Files
     "data": [
         "data/report_paperformat.xml",
         "security/ir.model.access.csv",
@@ -23,18 +28,17 @@
         "views/crm_lead_views.xml",
         "views/sale_order_views.xml",
         "views/res_company_views.xml",
-        # Report
+        # Reports
         "reports/custom_invoice_layout.xml",
         "reports/report_invoice.xml",
         "reports/report_quotation.xml",
-        
         "reports/report_pro_invoice.xml",
         "reports/report_pro_quotation.xml",
-        
         "reports/report_action.xml",
     ],
 
+    # Installation Settings
     "installable": True,
     "application": False,
-    "auto_install": False
+    "auto_install": False,
 }

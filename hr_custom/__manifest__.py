efd6ff96401320d0a3f3e5e35f5d10adde559cd9
rkhattam1995 <rs.khattam@gmail.com>
2025-03-29 17:58:34 +0400

    Update __manifest__.py

--- hr_custom/__manifest__.py
+++ hr_custom/__manifest__.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -*-
 # Part of 4Minds. See LICENSE file for full copyright and licensing details.
 {
     "name": "HR Custom",
@@ -15,12 +15,12 @@
     "license": "LGPL-3",
 
     # Dependency
-    "depends": ['web', 'base','hr_contract'],
+    "depends": ['web', 'base', 'hr_contract', 'hr_attendance'],
 
     "data": [
-        
         "views/contract_view.xml",
-       
+        "views/hr_attendance_view.xml",  # Add this line to include the new view
+        "security/ir.model.access.csv",  # Add this line to include access rights
     ],
 
     "installable": True,

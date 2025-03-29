0e8d8691cabd4715e5595e3975e32f756148e31e
rkhattam1995 <rs.khattam@gmail.com>
2025-03-29 17:41:44 +0400

    Update hr_attendance.py

--- invoice_training_details/models/hr_attendance.py
+++ invoice_training_details/models/hr_attendance.py
@@ -1,6 +1,12 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class HrAttendance(models.Model):
     _inherit = 'hr.attendance'
 
     checkin_comment = fields.Text(string="Check-in Comment")
+
+    @api.model
+    def create(self, vals):
+        if 'check_in' in vals:
+            vals['checkin_comment'] = vals.get('checkin_comment', 'No comment')
+        return super(HrAttendance, self).create(vals)

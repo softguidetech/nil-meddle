922f0c2f2ca590e4293592417c1c8d617e100785
rkhattam1995 <rs.khattam@gmail.com>
2025-03-30 14:28:41 +0400

    Update crm_lead.py

--- invoice_training_details/models/crm_lead.py
+++ invoice_training_details/models/crm_lead.py
@@ -142,8 +142,9 @@ class Lead(models.Model):
             'default_uber' : self.uber,
             'default_ctrng': self.ctrng,
             'default_ins_time': self.ins_time,
-            
-
+            # Add ticket and hotel details
+            'default_ticket_ids': [(6, 0, self.ticket_ids.ids)],
+            'default_hotel_ids': [(6, 0, self.hotel_ids.ids)],
         })
         return quotation_context
 

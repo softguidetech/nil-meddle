26e8d5ca017157a1ff19840e06df5e137c59906c
rkhattam1995 <rs.khattam@gmail.com>
2025-05-02 17:15:26 +0400

    Update sale_order.py

--- invoice_training_details/models/sale_order.py
+++ invoice_training_details/models/sale_order.py
@@ -92,20 +92,7 @@ class SaleOrder(models.Model):
     ctrng = fields.Float(string='Catering')  # Now it's manually editable
     
     bank_details = fields.Html(string='Bank Details',default='We kindly request you to transfer OR deposit cheque payment to below bank account details </br> Account Name: NIL Data Communications Middle East DMCC Emirates Islamic Bank JLT Branch - Dubai- UAE </br> Swiftcode: MEBLAEAD </br> Account Currency: USD </br> IBAN: AE690340003528215597102')
-    term_and_cond = fields.Html(string='Term and conditions',default='Details:
- 
-End customer name: 
-Training Location:
-Payment: Net 60
-
-
-Please email your invoice per the above T&Cs
-Payment Terms:  Net60 after delivery Please notify us immediately if you are unable to deliver as specified.
-Send all correspondence to:
-yabosaleh@nil-me.com
-NIL ME – DMCC – JLT 
-Dubai - UAE
-)
+    term_and_cond = fields.Html(string='Term and conditions',default=' 1. PO Reference #: PCD-006-2024 </br> 2. PO Amendment PCD-006-2024 </br> 3. End customer name: Saudi Authority for Data and Artificial Intelligence, Saudi Arabia. </br>4. The invoice amount does not include VAT or Withholding tajes - it must be paid by Taqnia Cyber if any, without any charging or deduction from the invoice amount.5. Taqnia Cyber will pay the taxes to KSA authorities directly.</br> 6. Taqnia Cyber must bear Money transfers or bank charges on payment.</br>')
     
     @api.depends('pro_service_ids.price')
     def _compute_service_price(self):

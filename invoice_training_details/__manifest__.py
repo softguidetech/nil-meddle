d93ae497bd1018312d69de45d70e64291646b283
rkhattam1995 <rs.khattam@gmail.com>
2025-10-16 11:17:19 +0400

    Update report file reference in manifest

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -36,7 +36,7 @@
         "reports/report_pro_invoice.xml",  # Professional invoice report
         "reports/report_pro_quotation.xml",  # Professional quotation report
         "reports/report_action.xml",  # Custom action for reports
-        "reports/report_invoice_cash.xml",
+        "reports/report_invoice_KSA.xml",
     ],
 
     # Installation Settings

5c170b460a3ff39e37b388bea97eedaa4ae59cb9
rkhattam1995 <rs.khattam@gmail.com>
2026-09-04 11:42:43 +0300

    Update __manifest__.py

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -5,19 +5,31 @@
     "summary": "Enhancements for Invoice Training Details",
     "description": "Customizations for invoicing related to training services, including improved reporting and CRM integration.",
     "category": "Customization",
+
     "author": "SoftGuide Tech",
     "website": "https://www.softguidetech.com",
     "license": "LGPL-3",
-    "depends": ["account", "crm", "sale_management", "purchase", "hr", "product"],
+
+    "depends": [
+        "account",
+        "crm",
+        "sale_management",
+        "purchase",
+        "hr",
+        "product",
+    ],
+
     "data": [
         "data/report_paperformat.xml",
         "security/ir.model.access.csv",
+
         "views/account_move_views.xml",
         "views/lcp_details_views.xml",
         "views/crm_lead_views.xml",
         "views/sale_order_views.xml",
         "views/res_company_views.xml",
         "views/purchase_training_report.xml",
+
         "reports/custom_invoice_layout.xml",
         "reports/report_invoice.xml",
         "reports/report_quotation.xml",
@@ -26,14 +38,19 @@
         "reports/report_action.xml",
         "reports/report_invoice_KSA.xml",
     ],
+
     "assets": {
         "web.assets_backend": [
             "invoice_training_details/static/src/css/lcp_details.css",
+
             "invoice_training_details/static/src/css/nil_terms_editor.css",
+            "invoice_training_details/static/src/css/nil_layout_fix.css",
+
             "invoice_training_details/static/src/js/nil_terms_editor.js",
             "invoice_training_details/static/src/xml/nil_terms_editor.xml",
         ],
     },
+
     "installable": True,
     "application": False,
     "auto_install": False,

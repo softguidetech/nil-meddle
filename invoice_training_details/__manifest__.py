5a5fdb90eced1776b19cb6e7e29d2640ec055c1c
rkhattam1995 <rs.khattam@gmail.com>
2026-09-08 02:49:30 +0300

    Bump module version for Koenig PO hotfix

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Invoice Training Details",
-    "version": "17.0.1.25",
+    "version": "17.0.1.26",
     "summary": "Enhancements for Invoice Training Details",
     "description": "Customizations for invoicing related to training services, including improved reporting and CRM integration.",
     "category": "Customization",

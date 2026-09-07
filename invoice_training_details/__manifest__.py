9692512ba515a21a2442b434615c4625d3bac9de
rkhattam1995 <rs.khattam@gmail.com>
2026-09-07 22:03:44 +0300

    Register per-training LCP view and assets

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Invoice Training Details",
-    "version": "17.0.1.8",
+    "version": "17.0.1.9",
     "summary": "Enhancements for Invoice Training Details",
     "description": "Customizations for invoicing related to training services, including improved reporting and CRM integration.",
     "category": "Customization",
@@ -25,6 +25,7 @@
         "views/account_move_views.xml",
         "views/lcp_details_views.xml",
         "views/crm_lead_views.xml",
+        "views/lcp_per_training_views.xml",
         "views/sale_order_views.xml",
         "views/res_company_views.xml",
         "views/purchase_training_report.xml",
@@ -44,7 +45,9 @@
             "invoice_training_details/static/src/css/nil_layout_fix.css",
             "invoice_training_details/static/src/js/nil_terms_editor.js",
             "invoice_training_details/static/src/js/force_bottom_chatter.js",
+            "invoice_training_details/static/src/js/lcp_per_training_field.js",
             "invoice_training_details/static/src/xml/nil_terms_editor.xml",
+            "invoice_training_details/static/src/xml/lcp_per_training_field.xml",
         ],
     },
 

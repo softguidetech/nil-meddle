0cc1cc3df373962cbdd56680553d3f371f52811a
rkhattam1995 <rs.khattam@gmail.com>
2026-09-08 04:13:31 +0300

    Load direct instructor domain patch

--- invoice_training_details/__manifest__.py
+++ invoice_training_details/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     "name": "Invoice Training Details",
-    "version": "17.0.1.34",
+    "version": "17.0.1.35",
     "summary": "Enhancements for Invoice Training Details",
     "description": "Customizations for invoicing related to training services, including improved reporting and CRM integration.",
     "category": "Customization",
@@ -57,6 +57,7 @@
             "invoice_training_details/static/src/js/lcp_per_training_field.js",
             "invoice_training_details/static/src/xml/nil_terms_editor.xml",
             "invoice_training_details/static/src/xml/lcp_per_training_field.xml",
+            "invoice_training_details/static/src/xml/lcp_instructor_direct_domain_patch.xml",
             "invoice_training_details/static/src/xml/lcp_commercial_automation_patch.xml",
         ],
     },

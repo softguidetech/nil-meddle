34fddef6634865c3afe11150159d200a4a9d9926
rkhattam1995 <rs.khattam@gmail.com>
2026-09-08 03:39:17 +0300

    Enlarge EnterOne terms and include PO end customer

--- invoice_training_details/models/lcp_terms_footer.py
+++ invoice_training_details/models/lcp_terms_footer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from html import escape
+
 from odoo import models
 
 
@@ -18,8 +20,29 @@ TERMS_FOOTER = (
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
+    def _lcp_end_customer_html(self):
+        self.ensure_one()
+        end_customer = self.training_name.display_name if self.training_name else ''
+        return (
+            '<div style="font-family:Arial,Helvetica,sans-serif;font-size:18px;'
+            'line-height:1.4;margin-bottom:10px;">'
+            '<strong>End Customer:</strong> %s'
+            '</div>'
+        ) % escape(end_customer)
+
     def _lcp_enterone_so_terms(self, courses):
-        return super()._lcp_enterone_so_terms(courses) + TERMS_FOOTER
+        html = super()._lcp_enterone_so_terms(courses)
+        html = html.replace('font-size:16px', 'font-size:18px')
+        html = html.replace('padding:8px 11px', 'padding:10px 14px')
+        html = html.replace(
+            'width:auto;display:inline-table;',
+            'width:auto;min-width:520px;display:inline-table;'
+        )
+        return html + TERMS_FOOTER
 
     def _lcp_koenig_po_terms(self, courses):
-        return super()._lcp_koenig_po_terms(courses) + TERMS_FOOTER
+        return (
+            self._lcp_end_customer_html()
+            + super()._lcp_koenig_po_terms(courses)
+            + TERMS_FOOTER
+        )

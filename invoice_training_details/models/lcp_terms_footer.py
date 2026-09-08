853d616a3d2e53a3c3c6b157720529629caa8381
rkhattam1995 <rs.khattam@gmail.com>
2026-09-08 03:38:36 +0300

    Format payment terms as heading and bullets

--- invoice_training_details/models/lcp_terms_footer.py
+++ invoice_training_details/models/lcp_terms_footer.py
@@ -4,10 +4,13 @@ from odoo import models
 
 
 TERMS_FOOTER = (
-    '<div style="font-family:Arial,Helvetica,sans-serif;font-size:16px;'
-    'line-height:1.45;margin-top:14px;">'
-    '<div><strong>Payment Terms:</strong> Net60 after delivery.</div>'
-    '<div>Please notify us immediately if you are unable to deliver as specified.</div>'
+    '<div style="font-family:Arial,Helvetica,sans-serif;font-size:18px;'
+    'line-height:1.45;margin-top:16px;">'
+    '<div style="font-weight:700;margin-bottom:4px;">Payment Terms:</div>'
+    '<ul style="margin:0;padding-left:22px;">'
+    '<li style="margin:0 0 3px 0;">Net60 after delivery.</li>'
+    '<li style="margin:0;">Please notify us immediately if you are unable to deliver as specified.</li>'
+    '</ul>'
     '</div>'
 )
 

c07c1fcfd03a1ba1a7a8f1c0cc56f50987481b2a
rkhattam1995 <rs.khattam@gmail.com>
2026-09-17 14:46:41 +0300

    Load EnterOne vendor and SO logistics final fixes

--- invoice_training_details/models/__init__.py
+++ invoice_training_details/models/__init__.py
@@ -21,3 +21,5 @@ from . import lcp_terms_footer
 from . import lcp_payment_method_preserve
 from . import lcp_snapshot_compat
 from . import training_snapshot
+from . import lcp_enterone_vendor_fix
+from . import so_logistics_fix
